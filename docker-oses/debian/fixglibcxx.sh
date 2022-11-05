02235be9faad2c654b6e1f74d88d06369f0349d0
Falaxir <61189667+Falaxir@users.noreply.github.com>
2022-11-05 14:55:17 +0100

    Update fixglibcxx.sh

--- docker-oses/debian/fixglibcxx.sh
+++ docker-oses/debian/fixglibcxx.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 echo "deb http://ftp.fr.debian.org/debian/ testing main contrib non-free" >> /etc/apt/sources.list
 echo "deb-src http://ftp.fr.debian.org/debian/ testing main contrib non-free" >> /etc/apt/sources.list
 tee -a /etc/apt/preferences << END
