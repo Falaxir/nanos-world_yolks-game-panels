b332d7d6fad0a13165c1e0e0f0976bcf3b0f4437
Falaxir <61189667+Falaxir@users.noreply.github.com>
2022-11-05 14:39:17 +0100

    Create fixglibcxx.sh
    Used to fix `version `GLIBCXX_3.4.30' not found`

---
+++ docker-oses/debian/fixglibcxx.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "deb http://ftp.fr.debian.org/debian/ testing main contrib non-free" >> /etc/apt/sources.list
+echo "deb-src http://ftp.fr.debian.org/debian/ testing main contrib non-free" >> /etc/apt/sources.list
+tee -a /etc/apt/preferences << END
+Package: *
+Pin: release a=stable
+Pin-Priority: 900
+
+Package: *
+Pin: release a=testing
+Pin-Priority: 400
+END
+apt update
+apt-get --target-release testing install libstdc++6
